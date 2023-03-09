2f407d982789cd5a777e0ed391ab475ff97b786b
bynico1 <nicofoque0205@gmail.com>
2023-03-09 11:42:33 -0500

    deploy

---
+++ src/Machines/bookingMachine.js
@@ -0,0 +1,118 @@
+import { createMachine, assign } from "xstate";
+import { fetchCountries } from "../Utils/api";
+
+const fillCountries = {
+  initial: "loading",
+  states: {
+    loading: {
+      invoke: {
+        id: "getCountries",
+        src: () => fetchCountries,
+        onDone: {
+          target: "success",
+          actions: assign({
+            countries: (context, event) => event.data,
+          }),
+        },
+        onError: {
+          target: "failure",
+          actions: assign({
+            error: "Fallo el request",
+          }),
+        },
+      },
+    },
+    success: {},
+    failure: {
+      on: {
+        RETRY: { target: "loading" },
+      },
+    },
+  },
+};
+
+const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    context: {
+      passengers: [],
+      selectedCountry: "",
+      countries: [],
+      error: "",
+    },
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+          },
+        },
+      },
+      search: {
+        on: {
+          CONTINUE: {
+            target: "passengers",
+            actions: "choiceSelectedCountry",
+          },
+          CANCEL: "initial",
+        },
+        ...fillCountries,
+      },
+      tickets: {
+        after: {
+          5000: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+        },
+        on: {
+          FINISH: {
+            target: "initial",
+            actions: assign({
+              selectedCountry: "",
+              passengers: [],
+            }),
+          },
+        },
+      },
+      passengers: {
+        on: {
+          DONE: {
+            target: "tickets",
+            cond: "moreThanOnePassenger",
+          },
+          CANCEL: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+          ADD: {
+            target: "passengers",
+            actions: "addPassenger",
+          },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      addPassenger: assign((context, event) =>
+        context.passengers.push(event.newPassenger)
+      ),
+      choiceSelectedCountry: assign({
+        selectedCountry: (context, event) => event.selectedCountry,
+      }),
+      cleanContext: assign({
+        selectedCountry: "",
+        passengers: [],
+      }),
+    },
+    guards: {
+      moreThanOnePassenger: (context) => {
+        return context.passengers.length > 0;
+      },
+    },
+  }
+);
+
+export default bookingMachine;
